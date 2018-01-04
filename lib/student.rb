a90fcf095290b6caf2508c3acfb41b47d821f1b2
Adjoa Darien <adjoa.darien@gmail.com>
2018-01-04 19:52:39 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,18 +12,21 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    students_array.each.tap do |student|
+    students_array.each do |student|
       self.new(student)
     end
   end
 
   def add_student_attributes(attributes_hash)
+    self.tap do |student_attr|
     self.bio = attributes_hash[:bio]
     self.blog = attributes_hash[:blog]
     self.profile_quote = attributes_hash[:profile_quote]
     self.linkedin = attributes_hash[:linkedin]
     self.twitter = attributes_hash[:twitter]
     self.github = attributes_hash[:github]
+  end
+    
   end
 
   def self.all
