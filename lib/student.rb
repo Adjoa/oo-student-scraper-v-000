91f98254f751bbd7fe71b514627d06147c59b92a
Adjoa Darien <adjoa.darien@gmail.com>
2018-01-04 19:53:03 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,14 +19,13 @@ class Student
 
   def add_student_attributes(attributes_hash)
     self.tap do |student_attr|
-    self.bio = attributes_hash[:bio]
-    self.blog = attributes_hash[:blog]
-    self.profile_quote = attributes_hash[:profile_quote]
-    self.linkedin = attributes_hash[:linkedin]
-    self.twitter = attributes_hash[:twitter]
-    self.github = attributes_hash[:github]
-  end
-    
+      student_attr.bio = attributes_hash[:bio]
+      student_attr.blog = attributes_hash[:blog]
+      student_attr.profile_quote = attributes_hash[:profile_quote]
+      student_attr.linkedin = attributes_hash[:linkedin]
+      student_attr.twitter = attributes_hash[:twitter]
+      student_attr.github = attributes_hash[:github]
+    end
   end
 
   def self.all
